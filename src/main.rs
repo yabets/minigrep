36f57a86d007201e843ed47bffe59e9a80926096
Yabi <yabetsbelay@gmail.com>
2025-06-08 20:19:38 +0200

    Parse command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(1);
+    }
 }
+
